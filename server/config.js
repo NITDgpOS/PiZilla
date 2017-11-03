7f96c9171af2c5d253a565cee20a06d7139385f9
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2017-11-04 03:21:53 +0530

    Add uploads folder with gitkeep

--- server/config.js
+++ server/config.js
@@ -3,6 +3,9 @@ import path from 'path';
 const config = {
     deadline: 24, // hours beyond which a file is deleted
     deleteSchedule: '00 00 */2 * * *', // runs the check once every two hours
+    excludedFiles: [
+        '.gitkeep'
+    ],
     port: 8000,
     root: path.dirname(__dirname),
     uploads: 'uploads'
--- server/scheduler.js
+++ server/scheduler.js
@@ -1,10 +1,19 @@
 import { readdirAsync, statAsync, unlinkAsync } from './utils';
+import minimatch from 'minimatch';
 import path from 'path';
 import serverConfig from './config';
 
 const cleanUp = async (dir) => {
     try {
-        const files = await readdirAsync(dir);
+        let files = await readdirAsync(dir);
+        // filter all excluded files
+        files = files.filter((file) => {
+            const checks = serverConfig.excludedFiles.map(
+                (pattern) => minimatch(file, pattern)
+            );
+            return checks.every((val) => val === false);
+        });
+
         files.forEach(async (file) => {
             const stat = await statAsync(path.join(dir, file));
             const deadline = Date.now() - Date.parse(stat.mtime);

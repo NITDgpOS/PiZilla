76c9052a00dee73699e132f47c67cd752dccb69b
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2017-11-04 05:57:46 +0530

    Remove unused dependencies
    - Use babel-core for compilation
    - Remove babel-register and babel-polyfill
    
    Fixes https://github.com/NITDgpOS/PiZilla/issues/51

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -51,7 +51,7 @@ const config = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
+        path: path.resolve(__dirname, 'public', 'client')
     },
     plugins: [
         new webpack.optimize.OccurrenceOrderPlugin(),

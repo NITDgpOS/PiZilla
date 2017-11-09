1029af8f4ee6b7ef051f5e7d2075a999a3b83b77
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2017-11-10 01:34:55 +0530

    Add Fork me on GitHub ribbon

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -12,11 +12,20 @@ class App extends Component {
 
     render = () => {
         const { store } = this.props;
+        const forkImgProps = {
+            alt: 'Fork me on GitHub',
+            'data-canonical-src': 'https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png',
+            src: 'https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67',
+            style: { border: 0, position: 'absolute', right: 0, top: 0 }
+        };
         return (
             <main>
                 <SideBar { ...store } />
                 <DropZone path={ store.path } />
                 <Footer />
+                <a href="https://github.com/NITDgpOS/PiZilla">
+                    <img { ...forkImgProps } />
+                </a>
             </main>
         );
     }
